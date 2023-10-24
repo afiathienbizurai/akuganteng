98f098b5b79e74a00a92e50d376a71ef0c92885f
afiathienbizurai <afiathienbizurai>
2023-10-24 19:58:48 +0800

    first commit

---
+++ operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+int a=1;
+int b=2;
+int c=20;
+int d=a+b+c;
+printf("%d\n",d+d);
+    return 0;
+}
---
+++ scanf_putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+b = getchar ();
+scanf("%c",&b);
+printf("%d",a);
+printf("%d",b);
+
+
+    return 0;
+}
